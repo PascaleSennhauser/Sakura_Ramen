16ea7c339c2b7c4bd492a5508ddbb3e64a6ac418
PascaleSennhauser <153015334+PascaleSennhauser@users.noreply.github.com>
2024-03-02 15:08:38 +0100

    Outsource javascript to script.js

---
+++ script.js
@@ -0,0 +1,44 @@
+function german(){
+    document.getElementById('jsMenu').innerHTML = 'MENÜ';
+    document.getElementById('jsLocations').innerHTML = 'STANDORT';
+    document.getElementById('jsAbout').innerHTML = 'ÜBER UNS';
+    document.getElementById('headlineDesc').innerHTML = 'DIE BESTEN RAMEN IN DER STADT';
+    document.getElementById('jsHtoTitle').innerHTML = 'BESTELLVORGANG';
+    document.getElementById('hto1').innerHTML = `Wähle deine<br> Nudeln`;
+    document.getElementById('hto2').innerHTML = `Wähle deine<br> Brühe`;
+    document.getElementById('hto3').innerHTML = `Wähle deine<br> Toppings`;
+    document.getElementById('jsOmTitle').innerHTML = 'UNSER MENÜ';
+    document.getElementById('jsChicken').innerHTML = 'Huhn';
+    document.getElementById('jsBeef').innerHTML = 'Rind';
+    document.getElementById('jsShrimp').innerHTML = 'Shrimp';
+    document.getElementById('jsChicken2').innerHTML = 'Huhn';
+    document.getElementById('jsBeef2').innerHTML = 'Rind';
+    document.getElementById('jsShrimp2').innerHTML = 'Shrimp';
+    document.getElementById('jsChicken3').innerHTML = 'Huhn';
+    document.getElementById('jsBeef3').innerHTML = 'Rind';
+    document.getElementById('jsShrimp3').innerHTML = 'Shrimp';
+    document.getElementById('jsFuaTitle').innerHTML = 'UNSER STANDORT';
+}
+
+
+function english(){
+    document.getElementById('jsMenu').innerHTML = 'MENU';
+    document.getElementById('jsLocations').innerHTML = 'LOCATIONS';
+    document.getElementById('jsAbout').innerHTML = 'ABOUT';
+    document.getElementById('headlineDesc').innerHTML = 'THE BEST RAMEN IN TOWN';
+    document.getElementById('jsHtoTitle').innerHTML = 'HOW TO ORDER';
+    document.getElementById('hto1').innerHTML = `Pick your<br> noodle`;
+    document.getElementById('hto2').innerHTML = `Pick your<br> broth`;
+    document.getElementById('hto3').innerHTML = `Pick your<br> toppings`;
+    document.getElementById('jsOmTitle').innerHTML = 'OUR MENU';
+    document.getElementById('jsChicken').innerHTML = 'Chicken';
+    document.getElementById('jsBeef').innerHTML = 'Beef';
+    document.getElementById('jsShrimp').innerHTML = 'Shrimp';
+    document.getElementById('jsChicken2').innerHTML = 'Chicken';
+    document.getElementById('jsBeef2').innerHTML = 'Beef';
+    document.getElementById('jsShrimp2').innerHTML = 'Shrimp';
+    document.getElementById('jsChicken3').innerHTML = 'Chicken';
+    document.getElementById('jsBeef3').innerHTML = 'Beef';
+    document.getElementById('jsShrimp3').innerHTML = 'Shrimp';
+    document.getElementById('jsFuaTitle').innerHTML = 'FIND US AT';
+}
